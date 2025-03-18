4d7da51a045a695d1a23b61fb93dfe683f3f9b51
Fedor1kaaaa <fedorka000aa@gmail.com>
2025-03-18 11:27:08 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let welcomeText = document.querySelector(".welcome-text");
-
-    // Добавляем класс с анимацией после небольшой задержки
-    setTimeout(() => {
-        welcomeText.style.opacity = "1";
-        welcomeText.style.transform = "translateY(0)";
-    }, 500);
-});
 function showServerInfo() {
     document.getElementById('server-info').classList.remove('hidden');
 }
