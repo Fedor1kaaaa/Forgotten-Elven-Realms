e3462ab1dc27cea48114ede4c341b40319a254ff
Fedor1kaaaa <fedorka000aa@gmail.com>
2025-03-18 21:44:38 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-document.getElementById('server-btn').addEventListener('click', function() {
-    let info = document.getElementById('server-info');
-    info.style.display = 'block';
-    info.style.opacity = '0';
-    info.style.transition = 'opacity 1s';
-    setTimeout(() => info.style.opacity = '1', 10);
-});
+function showServerInfo() {
+    document.getElementById("server-info").classList.add("show");
+}
