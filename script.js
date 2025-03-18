fffc73a0d008d54bc4049f3fc70efe4ca9ed8177
Fedor1kaaaa <fedorka000aa@gmail.com>
2025-03-18 09:48:03 +0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function showServerInfo() {
+    var serverInfo = document.getElementById("server-info");
+    serverInfo.style.opacity = "1"; // Делаем блок видимым
+    serverInfo.style.transform = "translateY(0)"; // Убираем сдвиг вверх
+}
